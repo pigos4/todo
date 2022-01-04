091f7695b2b1ad5f09681ecaacd1f5021d260426
Daniel <pigos487@gmail.com>
2022-01-04 14:36:15 +0000

    package.json created5

--- server/index.ts
+++ server/index.ts
@@ -4,6 +4,27 @@ const bodyParser = require("body-parser");
 const app = express();
 const port = 4000;
 const dbMethods = require("./database/databasepouchdb");
+const proxy = require("http-proxy-middleware");
+//require("./routes")(app);
+module.exports = function (app:any) {
+  // add other server routes to path array
+  app.use(proxy(["/api"], { target: "http://localhost:4000" }));
+};
+
+if (
+  process.env.NODE_ENV === "production" ||
+  process.env.NODE_ENV === "staging"
+) {
+  // Add production middleware such as redirecting to https
+
+  // Express will serve up production assets i.e. main.js
+  app.use(express.static(__dirname + "/client/build"));
+  // If Express doesn't recognize route serve index.html
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 app.use(cors({ origin: "*" }));
 app.use(bodyParser.json());
 
@@ -28,6 +49,11 @@ app.post("/deleteitem", (req: any, res: any) => {
   dbMethods.deleteItem(req.body.id).then((x: any) => res.send(x));
 });
 
-app.listen(port, () => {
-  console.log(`App listen on port ${port}.`);
-});
+const PORT = process.env.PORT || 4000; //Heroku sets port dynamically
+app
+  .listen(PORT, () => {
+    console.log("listening...");
+  })
+  .on("error", (err) => {
+    console.log(`Error Code: ${err}`);
+  });
